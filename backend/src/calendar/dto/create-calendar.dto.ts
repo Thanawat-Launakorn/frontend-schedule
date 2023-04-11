e10b9f04f312b8932e32b2b6c5dc18169656f924
Thanawat-Launakorn <everlastbw@hotmail.com>
2023-04-11 14:29:16 +0700

    delete

--- backend/src/calendar/dto/create-calendar.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateCalendarDto {
-  @ApiProperty()
-  date: string;
-}
--- backend/src/noti-line/dto/update-noti-line.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateNotiLineDto } from './create-noti-line.dto';
-
-export class UpdateNotiLineDto extends PartialType(CreateNotiLineDto) {}
